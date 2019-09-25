04f4093940d310ddf0314012e7a9572d9a809fed
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-09-25 10:30:03 +0800

    add comment

--- example/socket/socket-iot.js
+++ example/socket/socket-iot.js
@@ -12,13 +12,16 @@ var server = app.listen(3000, function() {
 
 var sio = io(server);
 
-// johnny-five object
+// johnny-five event when johnny init ready
 board.on('ready', function() {
+  // 指定LED output 為 Arduino 第13腳
   var led = new five.Led(13);
+  // led 初始化狀態
   led.off();
-  // socket.io work
+  // socket連線成功時，開始偵聽前端的 swEvent 事件
   sio.on('connection', function(socket) {
     socket.on('swEvent', function(data) {
+      //如果前端有動作則呼叫 johnny-five led.toggle() 切換led狀態
       console.log(data);
       if (data != false) {
         led.toggle();
