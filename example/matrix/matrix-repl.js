d391232dd47ceb56461910b76847466a90584e4a
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-09-26 12:00:17 +0800

    test ok

--- example/matrix/matrix-repl.js
+++ example/matrix/matrix-repl.js
@@ -8,43 +8,44 @@ board.on('ready', function() {
       clock: 3,
       cs: 4,
     },
-    devices: 1,
+    devices: 2,
   });
 
+  matrix.on().brightness(20);
+
   this.repl.inject({
-    on: function() {
-      console.log('matrix on!');
-      matrix.off();
-      matrix.on();
+    on: function(i) {
+      console.log('matrix device: ' + i + '  on');
+      matrix.off(i);
+      matrix.on(i);
     },
-    off: function() {
-      console.log('matrix off!');
-      matrix.stop();
-      matrix.off();
+    off: function(i) {
+      console.log('matrix device: ' + i + ' is off');
+      matrix.off(i);
     },
-    clear: function() {
-      console.log('matrix clear');
-      matrix.clear();
+    clear: function(i) {
+      console.log('matrix device: ' + i + ' is clear');
+      matrix.clear(i);
     },
-    brightness: function() {
-      console.log('matrix brightness');
-      matrix.brightness();
+    brightness: function(i, per) {
+      console.log(i + ' matrix device,brightness:' + per + '%');
+      matrix.brightness(i, per);
     },
-    led: function() {
-      console.log(' matrix led');
-      matrix.led();
+    led: function(i, row, col, state) {
+      console.log('第' + i + '台\nrow:' + row + ',col:' + col + ',狀態：');
+      matrix.led(i, row, col, state);
     },
-    row: function() {
-      console.log(' matrix row');
-      matrix.row();
+    row: function(i, row, num) {
+      console.log(i + ',' + row + ',' + num);
+      matrix.row(i, row, num);
     },
-    column: function() {
-      console.log(' matrix column');
-      matrix.column();
+    column: function(i, col, num) {
+      console.log(i + ',' + col + ',' + num);
+      matrix.column(i, col, num);
     },
-    draw: function() {
-      console.log(' matrix draw');
-      matrix.draw();
+    draw: function(i, char) {
+      console.log('第' + i + '台\n字元：' + char);
+      matrix.draw(i, char);
     },
   });
 });
