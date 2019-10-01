746325bf178ceded4438867a3f453f6feb399691
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-10-01 16:30:31 +0800

    change code

--- example/thermometer/thermometer.js
+++ example/thermometer/thermometer.js
@@ -4,11 +4,16 @@ var board = new five.Board();
 board.on('ready', function() {
   var temperature = new five.Thermometer({
     controller: 'LM35',
-    pin: 'A0',
-    freq: 1000,
+    pin: 'A0', // 類比輸入腳
+    freq: 1000, // 一秒取一次溫度值
   });
   temperature.on('data', function() {
     console.log(this.celsius + '°C');
     temp = this.celsius;
+    // 取攝氏溫度
+    if (temp == 30) {
+      // 當溫度 30 度時停止偵測動作
+      temperature.disable();
+    }
   });
 });
