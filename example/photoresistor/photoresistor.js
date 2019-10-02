bcf2bf4db41a0e249379a6309b9d0b11a16935a3
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-10-02 20:20:43 +0800

    修改code

--- example/photoresistor/photoresistor.js
+++ example/photoresistor/photoresistor.js
@@ -2,21 +2,14 @@ var five = require('johnny-five');
 var board = new five.Board();
 
 board.on('ready', function() {
-  // Create a new `photoresistor` hardware instance.
+  // new 一個 Johnny-Five Sensor 物件，宣告為 photoresistor
   photoresistor = new five.Sensor({
     pin: 'A0',
-    freq: 250,
+    freq: 500,
   });
 
-  // Inject the `sensor` hardware into
-  // the Repl instance's context;
-  // allows direct command line access
-  board.repl.inject({
-    pot: photoresistor,
-  });
-
-  // "data" get the current reading from the photoresistor
   photoresistor.on('data', function() {
+    // 讀取光敏電阻的值
     console.log(this.value);
   });
 });
