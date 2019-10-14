3cfc1735f43343d51396c3973ebb92ecc2105cc5
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-10-14 11:04:50 +0800

    add j-game

--- example/joystick/www/index.js
+++ example/joystick/www/index.js
@@ -1,13 +1,28 @@
 var socket = io.connect();
+var xCenter = $('.bg').width() / 2;
+var yCenter = $('.bg').height() / 2;
 
 socket.on('startData', function(data) {
   // 當socket開始連線時，接收資料
   // console.log(data);
-  tempData = data.pVal;
-  topPosition = tempData / 10;
-  console.log(topPosition);
-  if (topPosition >= 100) {
-    topPosition = 100;
-  }
-  $('.bear').css('top', '' + topPosition + '%');
+  XAxis = data.jXAxis;
+  YAxis = data.jYAxis;
+  XVal = XAxis.toFixed(2);
+  YVal = YAxis.toFixed(2);
+
+  varXVal = XVal * xCenter;
+  bearXPos = xCenter - varXVal;
+
+  varYVal = YVal * yCenter;
+  bearYPos = yCenter - varYVal;
+  $('.bear').css(
+    'transform',
+    'translate(' + bearXPos + 'px, ' + bearYPos + 'px',
+  );
+
+  // console.log(xCenter);
+  // console.log(yCenter);
+
+  console.log(bearXPos);
+  console.log(bearYPos);
 });
