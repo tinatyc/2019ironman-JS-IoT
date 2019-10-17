4b78cefb929e2b823c49130398ed23dbbc602b57
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2019-10-17 10:26:27 +0800

    fixed

--- example/joystick/www/index.js
+++ example/joystick/www/index.js
@@ -8,14 +8,17 @@ socket.on('startData', function(data) {
   // console.log(data);
   XAxis = data.jXAxis;
   YAxis = data.jYAxis;
-  XVal = XAxis.toFixed(2);
-  YVal = YAxis.toFixed(2);
 
-  varXVal = XVal * xCenter;
-  bearXPos = xCenter + varXVal;
+  XVal = XAxis.toFixed(2); // 取到小數點後兩位數
+  YVal = YAxis.toFixed(2); // 取到小數點後兩位數
 
-  varYVal = YVal * yCenter;
-  bearYPos = yCenter + varYVal;
+  varXVal = XVal * xCenter; // 取 joystick X軸移動量，轉成視窗元素 X軸 移動量
+  bearXPos = xCenter + varXVal; // 熊俠 X軸 的位置
+
+  varYVal = YVal * yCenter; // 取 joystick Y軸移動量，轉成視窗元素 Y軸 移動量
+  bearYPos = yCenter + varYVal; // 熊俠 Y軸 的位置
+
+  // 使用 jQ .css()函式改變熊俠位置
   $('.bear').css(
     'transform',
     'translate(' + bearXPos + 'px, ' + bearYPos + 'px',
