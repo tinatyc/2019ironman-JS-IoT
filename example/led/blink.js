a15d8fc5bb2a8dfff9db7490ffd1835d9d45f277
Tzeng Ying-Chi <tina_tyc@livemail.tw>
2020-07-24 01:17:53 +0800

    add gif for book

--- example/led/blink.js
+++ example/led/blink.js
@@ -4,12 +4,12 @@ var five = require('johnny-five');
 var board = new five.Board();
 //宣告Arduino開發版
 
-board.on('ready', function() {
+board.on('ready', function () {
   //當Arduino開發版ready好，做以下動作
 
   var led = new five.Led(13);
-  //宣告led在開發版第13腳
+  //宣告LED在開發版第13腳
 
   led.blink(1000);
-  //每500毫秒 LED會亮→滅→亮→滅...持續的閃爍
+  //每1000毫秒 LED會亮→滅→亮→滅...持續的閃爍
 });
